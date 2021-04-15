f04a149ca1db4f6e61361ebcb1ddd8b91e566143
VMHDev <vmhpublic2017@gmail.com>
2021-04-15 10:43:39 +0700

    Header: Add Navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Col, Container, Row } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavLink,
+} from 'reactstrap';
+
 import './styles.scss';
 
 const Header = () => {
-  return (
-    <header className='header'>
-      <Container>
-        <Row className='justify-content-between'>
-          <Col xs='auto'>
-            <a
-              className='header__link header__title'
-              href='https://youtube.com/easyfrontend'
-              target='_blank'
-              rel='noopener noreferrer'>
-              Photos
-            </a>
-          </Col>
 
-          <Col xs='auto'>
-            <NavLink
-              exact
-              className='header__link'
-              to='/user/login'
-              activeClassName='header__link--active'>
-              Login
-            </NavLink>
-          </Col>
-        </Row>
-      </Container>
-    </header>
+  // Render GUI
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color='light' light expand='md'>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='mr-auto' navbar>
+            <NavbarBrand href='/'>PHOTO APPS</NavbarBrand>
+            <NavItem>
+              <NavLink href='/photos'>Photo</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/categories'>
+                Category
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+        <UncontrolledDropdown>
+          <DropdownToggle nav caret>
+            User Name
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>Account</DropdownItem>
+            <DropdownItem>Logout</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Navbar>
+    </div>
   );
 };
 
